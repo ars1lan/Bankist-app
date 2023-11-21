113e2f045b2cfb99b85540a58495d7465c4e75fd
Arslan Ali <sunnysheikh303@gmail.com>
2023-11-21 13:12:24 +0500

    added the data from array inot div

--- ProjectFiles/script.js
+++ ProjectFiles/script.js
@@ -61,16 +61,42 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//--------------------------Display All Movements to the Div
+const displayMovemnet = function (movement) {
+  containerMovements.innerHTML = '';
+  movement.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const Html = `<div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+  </div>`;
+    containerMovements.insertAdjacentHTML('afterbegin', Html);
+  });
+};
+displayMovemnet(account1.movements);
+
+//-------------------------------Create User Name
+const CreateUserName = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+CreateUserName(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
 /////////////////////////////////////////////////
